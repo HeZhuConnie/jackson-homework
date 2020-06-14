08e513be68662705afdae1a9535fbe86db12c41a
he.zhu <he.zhu@thgoughtworks.com>
2020-06-14 21:58:24 +0800

    change eventVO - custom serialise & deserialise

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,6 +1,21 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import java.io.IOException;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +32,34 @@ public class EventVo {
   private String id;
   private String name;
   private EventType type;
+  @JsonSerialize(using = localDateTimeSerializer.class)
+  @JsonDeserialize(using = localDateTimeDeserializer.class)
   private LocalDateTime time;
+  @JsonUnwrapped
   private UserVo user;
+
+  private static class localDateTimeSerializer extends StdSerializer<LocalDateTime>{
+
+    protected localDateTimeSerializer(){
+      super (LocalDateTime.class);
+    }
+
+    @Override
+    public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+      gen.writeNumber(value.toInstant(ZoneOffset.of("+8")).toEpochMilli());
+    }
+  }
+
+  private static class localDateTimeDeserializer extends StdDeserializer<LocalDateTime> {
+
+    protected localDateTimeDeserializer(){
+      super (LocalDateTime.class);
+    }
+
+    @Override
+    public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+      Instant instant = Instant.ofEpochMilli(p.getLongValue());
+      return LocalDateTime.ofInstant(instant, ZoneId.of("+8"));
+    }
+  }
 }
